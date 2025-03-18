a153a161495f58f17b00098d24ee3a51412b10e8
BT_2N8_21 <boryslavw@nycstudents.net>
2025-03-18 16:13:29 -0400

    added some plans

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,27 @@
 public class Main {
     //initial commit to github
+
+
+
+
+
+
+
+
+
+
+
+    // it will be a 9x9 grid with the player start in the middle, the non player characters
+    // spawn facing a certain direction with a higher probability to somewhat face the player
+    // the npcs attack directly in the way they are facing, either 5 or 6 block range, if an npc
+    // hits another npc they are counted as hit and taken out, everything has one health with the
+    // player included
+
+
+    // the player can attack in a 3x3 grid, they have to plan theres moves to not be hit
+
+
+    // maybe two modes separated by 2 different windows or maybe a reset window, after a certain amount
+    // of turns you unlock the boss battle, in which the player moves freely and has diagonal or expanding
+    // attacks launched at them
 }
---
+++ src/Sprite.java
@@ -0,0 +1,10 @@
+public class Sprite {
+
+    public Sprite() {}
+
+    public void isHit(boolean hit) {
+        // this ends the spire and causes it to dissapear hopefully
+    }
+
+
+}
